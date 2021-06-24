5273e9b350390822f336c5b4acedb6092ed7fac9
Sunidhi <sunidhij223@gmail.com>
2021-06-25 03:01:05 +0530

    .

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const express = require("express");
 const app = express();
-const socket = require("socket.io");
+const socketIO = require("socket.io");
 
-
-const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
-
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-
+let server = app.listen( process.env.PORT || 2000, function () {
+  console.log("Server is running");
+});
 
 app.use(express.static("public"));
 
