c2657c06d90c11e95f9d10e1e32ea4e33e862c51
Sunidhi <sunidhij223@gmail.com>
2021-06-25 02:54:10 +0530

    .

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 const socket = require("socket.io");
-const cors = require('cors');
 
-app.use(cors());
 
 let server = app.listen(process.env.PORT || 2000 , function () {
   console.log("Server Running");
